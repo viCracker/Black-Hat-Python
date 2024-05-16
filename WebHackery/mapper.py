40ef0a20f578be461e5fe7fda09b911450db89b1
Victor Mutiso <mutisovictor69@gmail.com>
2024-05-16 12:47:52 +0300

    Add files via upload

---
+++ WebHackery/mapper.py
@@ -0,0 +1,46 @@
+import contextlib
+import os
+import queue
+import requests
+import sys
+import threading
+import time
+
+FILTERED = [".jpg", ".gif", ".png", ".css"]
+TARGET = "http://boodelyboo.com/wordpress"
+THREADS = 10
+
+answers = queue.Queue()
+web_paths = queue.Queue()
+
+
+def gather_paths():
+    for root, _, files in os.walk('.'):
+        for fname in files:
+            if os.path.splitext(fname)[1] in FILTERED:
+                continue
+            path = os.path.join(root, fname)
+            if path.startswith('.'):
+                path = path[1:]
+            print(path)
+            web_paths.put(path)
+
+
+@contextlib.contextmanager
+def chdir(path):
+    """
+    On enter, change directory to specified path
+    on exit, change directoy back to original
+    """
+    this_dir = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(this_dir)
+
+
+if __name__ == "__main__":
+    with chdir("/home/vicracker/Wordpress/wordpress"):
+        gather_paths()
+    input("Press return to continue")

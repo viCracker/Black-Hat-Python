36f3717d04ef1661271c337a630210e7bbbe53dd
Victor Mutiso <mutisovictor69@gmail.com>
2024-05-16 21:18:22 +0300

    Add files via upload

--- WebHackery/mapper.py
+++ WebHackery/mapper.py
@@ -40,7 +40,38 @@ def chdir(path):
         os.chdir(this_dir)
 
 
+def test_remote():
+    while not web_paths.empty():
+        path = web_paths.get()
+        url = f"{TARGET}{path}"
+        time.sleep(2)
+        r = requests.get(url)
+        if r.status_code == 200:
+            sys.stdout.write('+')
+        else:
+            sys.stdout.write('x')
+        sys.stdout.flush()
+
+
+def run():
+    mythreads = list()
+    for i in range(THREADS):
+        print(f"spawning thread {i}")
+        t = threading.Thread(target=test_remote)
+        mythreads.append(t)
+        t.start()
+    
+    for thread in mythreads:
+        thread.join()
+
+
 if __name__ == "__main__":
     with chdir("/home/vicracker/Wordpress/wordpress"):
         gather_paths()
     input("Press return to continue")
+
+    run()
+    with open('myanswers.txt', 'w') as f:
+        while not answers.empty():
+            f.write(f"{answers.get()}\n")
+        print("done")
